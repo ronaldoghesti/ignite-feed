f635352591507f49f1fb5ee9b7d728e1001fd825
Ronaldo Ghesti <ronaldo.ghesti@outlook.com.br>
2022-11-09 23:53:01 -0300

    feat: add props component Post

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,47 @@ import { Sidebar } from "./components/Sidebar";
 import "./global.css";
 import styles from "./App.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl:
+        "https://images.unsplash.com/photo-1630766786489-7a6d63d576e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80",
+      name: "Ronaldo Ghesti",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl:
+        "https://plus.unsplash.com/premium_photo-1664392237200-3015d69c1e7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=758&q=80",
+      name: "Moises Silva",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+];
+
 export function App() {
   return (
     <>
@@ -13,11 +54,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Ronaldo Ghesti"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis distinctio animi cupiditate, modi sunt ipsum assumenda? Similique dolorum nobis quae placeat quo temporibus eius id, atque quod deleniti quidem ex."
-          />
-          <Post author="Diego Fernandes" content="Um novo post muito legal" />
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </>
